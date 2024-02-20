d30c76a9a69285ce4848f2d92f9ae1c47e44c588
Nah-Ki <mohammednaqui4@gmail.com>
2024-02-20 16:24:41 +0530

    commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string pangram = " The quick brown fox jumps over the lazy dog";
+
+string[] message = pangram.Split(' ');
+
+
+string[] newMessage = new string[message.Length];
+
+for (int i=0 ; i<message.Length ; i++)
+{
+    char[] letters =  message[i].ToCharArray();
+    Array.Reverse(letters);
+    
+    newMessage[i] = new string(letters);
+ 
+}
+
+string result = string.Join(" ",newMessage);
+Console.WriteLine(result);
